4526432a1460b54885b80aecdc0b2c1e3c57f7f7
= <abrahamkirschner1@gmail.com>
2023-03-20 21:03:17 +0000

    Stuff

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 const path = require('path');
 require('dotenv').config()
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 //Create MongoDB Client
@@ -14,31 +15,61 @@ const client = new MongoClient(uri, { useNewUrlParser: true });
 
 
 app.get("/", async (req, res) => {
-    const HTML = await getMainPage("You Suck")
-    res.send(HTML)
-    console.log("sent!")
+    // const HTML = await getMainPage("You Suck")
+    // res.send(HTML)
+    // console.log("sent!")
     // res.send("Hi")
+    res.redirect("/vote")
 })
 
 app.post("/vote", (req, res) => {
-    if (!req.query.id || !req.query.vote) {
-        res.redirect("/").send(getMainPage("Please enter a citizen ID and Vote"));
+    console.log(req.body.id)
+    if (req.body.id == null || req.body.vote == null) {
+        res.render(__dirname + "/public/index.ejs", {statusMessage: "Please enter your citizen ID and vote!"})
         return;
     }
-    const citizenID = req.query.id;
-    const vote = req.query.vote;
+    const citizenID = req.body.id;
+    const vote = req.body.vote;
     if (vote.toLowerCase() == "quantity1" || vote.toLowerCase() == "quantity2") {
-        res.redirect("/").send(getMainPage("Voted Successfully!"))
+        client.connect(process.env.MONGODB_STRING, async (err, db) => {
+            if (err) throw (err)
+            else {
+                 const voteCollection = db.db("WyattApp").collection("votes")
+                 const citizenCollection = db.db("WyattApp").collection("citizens")
+                 collection.findOne({ citizenID: citizenID }, async (err, result) => {
+                      if (err) throw (err)
+                      if (result != null) {
+                        if (result.hasVoted == true) {
+                            res.render(__dirname + "/public/index.ejs", {statusMessage: "You've already voted!"})
+                        }
+                           await db.close()
+                      } else {
+                           collection.updateOne({ citizenID: citizenID }, {
+                            $set: {
+                                hasVoted: true
+                            }
+                           }, async (err, result) => {
+                                if (err) throw (err)
+                                res.render(__dirname + "/public/index.ejs", {statusMessage: "Voted successfully! Check back later for the results!"})
+                                await db.close()
+                           })
+                      }
+                 })
+            }
+       })
     } else {
-        res.render("/public/index.ejs")
+        res.render(__dirname + "/public/index.ejs", {statusMessage: "Don't mess with the site!"})
     }
-
 })
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('started on port 3000');
 });
 
 function generateHTMLVotes(data) {
     return "hi!"
 }
+
+app.get("/vote", (req, res) => {
+    res.render(__dirname + "/public/index.ejs", {statusMessage: "Vote below!"})
+})
---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+// $('#voteForm').submit(function(e) {
+//     e.preventDefault();
+//     $.ajax({
+//          type: 'POST',
+//          url: '/vote',
+//          data: $(this).serialize(),
+//         //  beforeSend: //do something
+//         //  complete: //do something
+//         //  success: //do something for example if the request response is success play your animation...
+//     });
+ 
+//  })
